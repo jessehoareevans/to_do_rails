274818fc10c0a90f00c5fd84732c567b3269d94d
Dan Jacobs and Jesse Hoare Evans <dan+jesse@>
2017-06-26 11:53:56 -0700

    ability to add a task to a list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -23,9 +23,30 @@ class ListsController < ApplicationController
     end
   end
 
+  def edit
+    @list = List.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @list= List.find(params[:id])
+    if @list.update(list_params)
+      redirect_to lists_path
+    else
+      render :edit
+    end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
+end
+
+
+
   private
     def list_params
       params.require(:list).permit(:name)
     end
-    
 end
